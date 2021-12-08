8fe2f58b31709a3b494cfe134b6c4cfb0ea10052
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2021-12-08 00:00:19 -0600

    terminando nav y vista detalles desktop

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home'
+import Details from './components/Details'
+import Nav from './components/Nav'
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/:idproduct" component={Details}/>
       </Switch>
     </div>
   );
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import s from '../assets/styles/Nav.module.css'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+    return (
+        <header className={s.container}>
+            <nav className={s.options}>
+                <img className={s.logo} src="https://www.seekpng.com/png/detail/428-4289671_logo-e-commerce-good-e-commerce-logo.png" alt="logo ecommerce" />
+                <input className={s.search} type="text" placeholder="Ingrese el producto a buscar"></input>
+                <div className={s.buttons}>
+                    <Link to=""><button className={s.btn}>Log In</button></Link>
+                    <Link to=""><button className={s.btn}>Registrarse</button></Link>
+                </div>
+            </nav>
+            
+        </header>
+    )
+}
+
+export default Nav
