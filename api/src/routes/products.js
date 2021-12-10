776d28e5f3eed8e9ffedcf967446245fb33d1f3e
ElPugna <tomi.pugna@gmail.com>
2021-12-10 15:17:18 -0300

    getProducts route added

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,17 +1,21 @@
 const {Router} = require('express');
+const {getProducts} = require('../Controllers/RouterFunctions/Products/GetProducts');
 const {getProductById} = require('../Controllers/RouterFunctions/Products/getProductById');
 const {getProductBySearch} = require('../Controllers/RouterFunctions/Products/getProductBySearch');
 const {getProductByRankPrice} = require('../Controllers/RouterFunctions/Products/getProductByRankPrice');
 const router = Router();
 
 router.get('/:productID', getProductById);
-router.get('/', (req, res, next)=>{
+
+router.get('/',getProducts, (req, res, next)=>{
     const {search, minPrice, maxPrice} = req.query;
     if (search){
         return getProductBySearch(req,res,next)
     }
     if (minPrice && maxPrice){
         return getProductByRankPrice(req,res,next)
+    }else{
+      return getProducts(req, res, next)
     }
 });
 
