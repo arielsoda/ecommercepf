eabdc7b5ddc3ca1b1e6b496af900b91cdf9a3ae9
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2021-12-10 00:57:25 -0600

    implementando validacion

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home'
 import Login from './components/Login';
 import Details from './components/Details'
 import Nav from './components/Nav'
-
+import Register from './components/Register'
 function App() {
   return (
     <div className="App">
@@ -14,7 +14,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/detail/:idproduct" element={<Details/>}/>
         <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/register" element={<h1>Form de registro</h1>} />
+        <Route exact path="/register" element={<Register/>} />
       </Routes>
     </div>
   );
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React,{useState}  from 'react'
 import s from '../assets/styles/Register.module.css'
 function Register() {
+    let initialError={
+        name:'Ingresa un nombre',
+        lastname:'Ingresa un apellido',
+        email:'Ingresa un email valido',
+        pass:'Ingresa una contraseña',
+        tel:'Ingresa un telefono',
+        chterm:'Debe aceptar los terminos y condiciones para continuar con el registro',
+    }
+    let [data,setData] = useState({})
+
+    function handleChange(e) {
+        let name=e.target.name;
+        let value=e.target.value;
+        setData({
+            ...data,
+            [name]:value})
+    }
     return (
         <div className={s.container}>
             <h2 className={s.title}>Formulario de registro</h2>
@@ -8,27 +25,27 @@ function Register() {
             <form className={s.form}>
                 <div className={s.formGroup}>
                     <label for="name">Nombre</label>
-                    <input type="text" id="name" placeholder="Nombre"/>
+                    <input type="text" id="name" name="name" placeholder="Nombre"/>
                 </div>
                 <div className={s.formGroup}>
                     <label for="lastname">Apellidos</label>
-                    <input type="text" id="lastname" placeholder="Apellidos"/>
+                    <input type="text" id="lastname" name="lastname" placeholder="Apellidos"/>
                 </div>
                 <div className={s.formGroup}>
                     <label for="email">Email</label>
-                    <input type="text" id="email" placeholder="Email"/>
+                    <input type="text" id="email" name="email" placeholder="Email"/>
                 </div>
                 <div className={s.formGroup}>
                     <label for="pass">Contraseña</label>
-                    <input type="password" id="pass" placeholder="Contraseña"/>
+                    <input type="password" id="pass" name="pass" placeholder="Contraseña"/>
                 </div>
                 <div className={s.formGroup}>
                     <label for="tel">Telefono</label>
-                    <input type="tel" id="tel" placeholder="Telefono"/>
+                    <input type="tel" id="tel" name="tel" placeholder="Telefono"/>
                 </div>
                 <div className={s.formGTerm}>
                     <label for="chterm">
-                    <input type="checkbox" id="chterm"  value=""/>Acepto los terminos y condiciones</label>
+                    <input type="checkbox" id="chterm" name="chterm" value=""/>Acepto los terminos y condiciones</label>
                 </div>
                 <div className={s.formGroup}>
                     <button className={s.btn} type="submit" >Registrarse</button>
