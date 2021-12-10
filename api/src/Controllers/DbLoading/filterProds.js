77b30c2b180a7b9232518d71438101d91c57f775
ElPugna <tomi.pugna@gmail.com>
2021-12-10 18:26:27 -0300

    Thumbnail filter update

--- api/src/Controllers/DbLoading/filterProds.js
+++ api/src/Controllers/DbLoading/filterProds.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const axios = require('axios');
-const {ID_ENDPOINT} = process.env;
+const {ID_ENDPOINT,THUMBNAIL_ENDPOINT} = process.env;
 
 const filterProds =  async (product, category)=>{
   try{
@@ -9,13 +9,40 @@ const filterProds =  async (product, category)=>{
       title, 
       price, 
       condition,
-      thumbnail,
       attributes,
     } = product;
     
     const brand = product.attributes.filter(elem=>elem.id ==="BRAND")[0].value_name.toLowerCase();
     const otherData = await axios.get(`${ID_ENDPOINT}${id}`);
+    //TODO: Agregar Thumbnail 400x400 y 250x250 (Ambas medidas vienen en todas las imágenes)
+    /*
+    | Adentro de pictures aparece:
+    pictures": [
+    {
+      !"id": "796011-MLA47691058372_092021",
+      "url": "http://http2.mlstatic.com/D_796011-MLA47691058372_092021-O.jpg",
+      "secure_url": "https://http2.mlstatic.com/D_796011-MLA47691058372_092021-O.jpg",
+      "size": "387x500",
+      "max_size": "722x932",
+      "quality": ""
+    }
+    |https://api.mercadolibre.com/pictures/${id} Aparece:
+    "variations": [
+    {
+      !"size": "722x932",
+      !"url": "http://mla-s1-p.mlstatic.com/796011-MLA47691058372_092021-F.jpg",
+      "secure_url": "https://mla-s1-p.mlstatic.com/796011-MLA47691058372_092021-F.jpg"
+    },
 
+
+    */
+    //[Obtengo el tumbnail de 320x320 
+    const thumbnailId = otherData.data.pictures[0].id
+    //! console.log("THUMBNAIL ID: ", thumbnailId);
+    let thumbnailResponse = await axios.get(`${THUMBNAIL_ENDPOINT}${thumbnailId}`);
+    let thumbnail = thumbnailResponse.data.variations.find(el=>el.size==="320x320")
+    thumbnail = thumbnail.url;
+    
     const image = otherData.data.pictures.map(el=>el.url);
     const newAttributes = otherData.data.attributes.map(el=>{
       if(el.id!=="GTIN" || el.id!=="EXCLUSIVE_CHANNEL"||el.id!=="BRAND"){
