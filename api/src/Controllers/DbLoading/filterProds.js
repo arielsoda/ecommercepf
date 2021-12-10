dade95fab20ce495bdecbbae4315ae31ce213427
ElPugna <tomi.pugna@gmail.com>
2021-12-10 18:45:11 -0300

    Thumbnail filter update

--- api/src/Controllers/DbLoading/filterProds.js
+++ api/src/Controllers/DbLoading/filterProds.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const axios = require('axios');
-const {ID_ENDPOINT} = process.env;
+const {ID_ENDPOINT,THUMBNAIL_ENDPOINT} = process.env;
 
 const filterProds =  async (product, category)=>{
   try{
@@ -9,13 +9,20 @@ const filterProds =  async (product, category)=>{
       title, 
       price, 
       condition,
-      thumbnail,
       attributes,
     } = product;
     
     const brand = product.attributes.filter(elem=>elem.id ==="BRAND")[0].value_name.toLowerCase();
     const otherData = await axios.get(`${ID_ENDPOINT}${id}`);
 
+
+    //[Obtengo el tumbnail de 320x320 
+    const thumbnailId = otherData.data.pictures[0].id
+    //! console.log("THUMBNAIL ID: ", thumbnailId);
+    let thumbnailResponse = await axios.get(`${THUMBNAIL_ENDPOINT}${thumbnailId}`);
+    let thumbnail = thumbnailResponse.data.variations.find(el=>el.size==="320x320")
+    thumbnail = thumbnail.url;
+    
     const image = otherData.data.pictures.map(el=>el.url);
     const newAttributes = otherData.data.attributes.map(el=>{
       if(el.id!=="GTIN" || el.id!=="EXCLUSIVE_CHANNEL"||el.id!=="BRAND"){
