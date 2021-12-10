91cc311d5a17f923a58e4a828845c639fcdd4c2f
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2021-12-10 12:13:18 -0600

    cambiando diseño de ProductCard

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import s from '../assets/styles/ProductCard.module.css'
 function ProductCard({ id , name , price , image}) {
    return (
-      <div className={s.container}>
-         <div className={s.namecont}>
-            <p className={s.name}><strong>{name}</strong></p>
-         </div>
-         <div className={s.imgcont}>
-            <img className={s.img} src={image} alt={`Imagen de ${name}`}/>
-         </div>
-         <div className={s.actions}>
-            <button className={`${s.btn} `}>Comprar ahora</button>
-            <button className={`${s.btn}`}>Agregar al carrito</button>
+      <div className={s.card}> 
+         <div className={s.container}> 
+            <div className={s.imgcont}>
+               <img className={s.img} src={image} alt={`Imagen de ${name}`}/>
+            </div>
+            <div className={s.actions}>
+               <p className={s.price}>{`$${price} USD`}</p>
+               <button className={`${s.btn} `}>Comprar ahora</button>
+               <button className={`${s.btn}`}>Agregar al carrito</button>
+            </div>
+            <div className={s.namecont}>
+                  <p className={s.name}><strong>{name}</strong></p>
+            </div>
          </div>
       </div>
    )
