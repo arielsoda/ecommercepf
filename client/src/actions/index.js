99b5b444a25d3a24ae4886e8a8829fb8fc1d36d6
LoreLor <lorenadearmas01@gmail.com>
2021-12-11 14:36:36 -0300

    add_sorts

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,95 +1,124 @@
 import { GET_ALL_PRODUCTS, 
          GET_PRODUCT_BY_NAME,
-         GET_PRODUCT_DETAIL,
+         GET_PRODUCT_ID,
          GET_ALL_CATEGORIES,
          FILTER_PRODUCTS_BY_CATEGORY,
          FILTER_PRODUCTS_BY_PRICE,
          FILTER_PRODUCTS_BY_BRANDS,
-         SORT_PRODUCTS
-                             } from "./actionsTypes";
+         SORT_PRODUCTS,
+         CREATE_CATEGORY,
+         CREATE_PRODUCT,
+         FILTERS_CLEAR
+} from "./actionsTypes";
 import axios from 'axios';
 
-
-export function getAllProducts() {
-    return async function(dispatch){
-        try{
-            const products = await axios.get('http://localhost:3001/products');
-            return dispatch({
-                type: GET_ALL_PRODUCTS,
-                payload: products.data
-            });
-        }catch(err){
-            console.log(err)
+    export function getAllProducts() {
+        return async function(dispatch){
+            try{
+                const products = await axios.get('http://localhost:3001/products');
+                return dispatch({
+                    type: GET_ALL_PRODUCTS,
+                    payload: products.data
+                });
+            }catch(err){
+                console.log(err)
+            }
         }
-    }
-};
+    };
 
-export function getProductByName() {
-    return async function(dispatch){
-        try{
-            const product = await axios.get(`http://localhost:3001/products?name=''`)
-            return dispatch({
-                type: GET_PRODUCT_BY_NAME,
-                payload: product.data
+    export function getProductByName() {
+        return async function(dispatch){
+            try{
+                const product = await axios.get(`http://localhost:3001/products?name=''`)
+                return dispatch({
+                    type: GET_PRODUCT_BY_NAME,
+                    payload: product.data
+                })
+            }catch(err){
+                console.log(err)
+            }
+        }
+    };
+
+    export function getProductId(idProduct) {
+        return async function(dispatch){
+            try{
+                const detail= await axios.get(`http://localhost:3001/products/${idProduct}`)
+                return dispatch({
+                    type: GET_PRODUCT_ID,
+                    payload: detail.data
+                })     
+            }catch(err){
+                console.log(err)
+            }   
+        }
+    };
+
+    export function getCategories(){
+        return async function(dispatch){
+            try{
+                const categories= await axios.get('http://localhost:3001/categories')
+                return dispatch({
+                    type: GET_ALL_CATEGORIES,
+                    payload: categories.data
+                })
+            }catch(err){
+                console.log(err)
+            }
+        }
+    };
+
+
+    export function createCategory(payload){
+        return async function (dispatch){
+            const newCategory = await axios.post( ,payload)
+            return dispatch ({
+                type: CREATE_CATEGORY,
+                payload: newCategory
             })
-        }catch(err){
-            console.log(err)
         }
-    }
-};
+    };
 
-export function getProductDetail(idProduct) {
-    return async function(dispatch){
-        try{
-            const detail= await axios.get(`http://localhost:3001/products/${idProduct}`)
-            return dispatch({
-                type: GET_PRODUCT_DETAIL,
-                payload: detail.data
-            })     
-        }catch(err){
-            console.log(err)
-        }   
-    }
-};
-
-export function getCategories(){
-    return async function(dispatch){
-        try{
-            const categories= await axios.get('http://localhost:3001/categories')
-            return dispatch({
-                type: GET_ALL_CATEGORIES,
-                payload: categories.data
+    export function createProduct(payload){
+        return async function (dispatch){
+            const newProduct = await axios.post( ,payload)
+            return dispatch ({
+                type: CREATE_PRODUCT,
+                payload: newProduct
             })
-        }catch(err){
-            console.log(err)
+        }
+    };
+
+    export function filterByCategory(payload){
+        return {
+            type: FILTER_PRODUCTS_BY_CATEGORY,
+            payload
+        }
+    };
+
+    export function filterByPrice(min, max){ //ver si tengo ruta
+        return{
+            type: FILTER_PRODUCTS_BY_PRICE,
+            payload: {min, max}
         }
     }
-};
 
-export function filterByCategory(payload){
-    return {
-        type: FILTER_PRODUCTS_BY_CATEGORY,
-        payload
+    export function filterProductByBrand(payload){
+        return{
+            type: FILTER_PRODUCTS_BY_BRANDS,
+            payload
+        }
     }
-};
 
-export function filterByPrice(min, max){
-    return{
-        type: FILTER_PRODUCTS_BY_PRICE,
-        payload: {min, max}
-    }
-}
+    export function sortProducts(payload){
+        return {
+            type: SORT_PRODUCTS,
+            payload
+        }
+    };
 
-export function filterProductByBrand(payload){
-    return{
-        type: FILTER_PRODUCTS_BY_BRANDS,
-        payload
+    export function filtersClear(){
+        return {
+            type: FILTERS_CLEAR
+        }
     }
-}
-
-export function sortProducts(payload){
-    return {
-        type: SORT_PRODUCTS,
-        payload
-    }
-};
--- client/src/reducers/products.reducer.js
+++ client/src/reducers/products.reducer.js
@@ -5,7 +5,10 @@ import{ GET_ALL_PRODUCTS,
         FILTER_PRODUCTS_BY_CATEGORY,
         FILTER_PRODUCTS_BY_PRICE,
         FILTER_PRODUCTS_BY_BRANDS,
-        SORT_PRODUCTS
+        SORT_PRODUCTS,
+        CREATE_CATEGORY,
+        CREATE_PRODUCT,
+        FILTERS_CLEAR
     } from '../actions/actionsTypes'
 
 const initialState = {
@@ -23,22 +26,36 @@ export function productsReducer(state = initialState, action){
                 allProducts: action.payload,
                 filters: action.payload
             };
+
         case GET_PRODUCT_BY_NAME:
             return {
                 ...state,
                 filters: action.payload
             };
+
         case GET_PRODUCT_DETAIL:
             return {
                 ...state,
                 allProducts: action.payload,
                 productDetail: action.payload
-            };    
+            };   
+
         case  GET_ALL_CATEGORIES: 
             return {
                 ...state,
                 categories: action.payload
             };
+
+        case CREATE_CATEGORY:
+            return {
+                ...state
+            }
+
+        case CREATE_PRODUCT:
+            return {
+                ...state
+            }
+
         case FILTER_PRODUCTS_BY_CATEGORY:
             const filteredCategory = action.payload === 'All'
             ? state.allProducts
@@ -48,15 +65,16 @@ export function productsReducer(state = initialState, action){
                 ...state,
                 filters: filteredCategory
             };
+
         case FILTER_PRODUCTS_BY_PRICE:  //ver como viene de ruta
-            const filteredPrice = action.payload === 'All'
-            ? state.allProducts
-            : state.allProducts.filter((p) => p.price >= action.payload.min && p.price <= action.payload)
+            const filteredPrice = !action.payload ? state.allProducts
+            : state.allProducts.filter((p) => p.price >= action.payload.min && p.price <= action.payload.max)
             return {
                 ...state,
                 filters: filteredPrice
             }
-            case FILTER_PRODUCTS_BY_BRANDS: 
+
+        case FILTER_PRODUCTS_BY_BRANDS: 
             const filteredBrands= action.payload === 'All'
             ? state.allProducts
             : state.allProducts.filter((p) => p.brands === action.payload)
@@ -64,38 +82,44 @@ export function productsReducer(state = initialState, action){
                 ...state,
                 filters: filteredBrands
             };
-            case SORT_PRODUCTS:
-                let sorts;
-                 if(action.payload === 'All') sorts=  state.allProducts;
-                 if(action.payload === 'AZ'){  //alpha
-                      sorts = state.filters.sort((a,b) => {
-                         if(a.name > b.name) return 1;
-                         if(a.name < b.name) return -1;
-                         return 0;
-                     })
-                 }
-                 if(action.payload === 'ZA'){
-                    sorts = state.filters.sort((a,b) => {
-                         if(a.name < b.name) return 1;
-                         if(a.name > b.name) return -1;
-                         return 0;
-                     })
-                 }
-                 if(action.payload === 'Lower price'){  //num
-                      sorts = state.filters.sort((a,b) => {
-                         return   a.price - b.price;
-                     })      
-                 }
-                 if(action.payload === 'Highest price'){
-                     sorts = state.filters.sort((a,b) => {
-                       return  b.price - a.price;
-                     })      
-                 }
+
+        case SORT_PRODUCTS:
+            let sorts;
+            if(action.payload === 'All') sorts=  state.allProducts;
+            if(action.payload === 'A-Z'){  //alpha
+                sorts = state.filters.sort((a,b) => {
+                    if(a.name > b.name) return 1;
+                    if(a.name < b.name) return -1;
+                    return 0;
+                })
+            }
+            if(action.payload === 'Z-A'){
+                sorts = state.filters.sort((a,b) => {
+                    if(a.name < b.name) return 1;
+                    if(a.name > b.name) return -1;
+                    return 0;
+                })
+            }
+            if(action.payload === 'Lower_price'){  //num
+                sorts = state.filters.sort((a,b) => {
+                    return   a.price - b.price;
+                })      
+            }
+            if(action.payload === 'Highest_price'){
+                sorts = state.filters.sort((a,b) => {
+                    return  b.price - a.price;
+                })      
+            }
+        return {
+            ...state,
+            filters: sorts
+        };
+
+        case FILTERS_CLEAR:
             return {
                 ...state,
-                filters: sorts
-            };
-     
+                filters: allProducts
+            }
 
         default:
             return state;
