dd448a3272fe62c723a96db9c001b188141dd7ba
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2021-12-12 15:21:34 -0600

    corrigiendo estilos de home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ import {useDispatch , useSelector} from 'react-redux';
 import { getAllProducts, getProductByName } from '../actions/index.js'
 import { useParams } from "react-router-dom";
 import Filters from "./Filters.jsx";
-import imgnotfound from "../assets/img/notfound.png";
+import imgnotfound from "../assets/img/notfound.gif";
 
 
 const Home = () => {
@@ -14,6 +14,8 @@ const Home = () => {
             if(Array.isArray(state.productsReducer.allProducts)) return state.productsReducer.allProducts;
         return state.productsReducer.allProducts.productsInfo})
     const {search=null} = useParams();
+
+    
     useEffect(()=>{
         console.log(search)
         if(!search)dispatch(getAllProducts())
@@ -26,8 +28,8 @@ const Home = () => {
     return (
         <div className={s.container}>
             <Filters/>
+            {search?<div className={s.search}><p>Resultados de busqueda de: <strong>{search}</strong></p></div>:null}
             <div className={s.cards}>
-                {search?<div className={s.search}><p>Resultados de busqueda de: <strong>{search}</strong></p></div>:null}
                 {Array.isArray(products)&&products.length>0?products.map((prod,i)=><Card key={i} id={prod.idProduct} name={prod.name} price={prod.price} image={prod.thumbnail}/>)
                 :<div className={s.notfound}>
                     <img className={s.imgfound} src={imgnotfound}/>
