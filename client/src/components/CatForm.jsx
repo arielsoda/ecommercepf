dfc284bbb2e58f30afd4ce139f80e84027891849
ElianZK <elianh2015@gmail.com>
2021-12-14 16:57:31 -0300

    falta arregrlar subida de datos

--- client/src/components/CatForm.jsx
+++ client/src/components/CatForm.jsx
@@ -1,4 +1,4 @@
-import { getCategories, createCategory } from "../actions/index";
+import { getProductsCategories, createCategory } from "../actions/index";
 import React,{useEffect, useState} from "react";
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
@@ -7,70 +7,44 @@ import s from '../assets/styles/CatForm.module.css';
 
 export default function CatForm (){
     const dispatch = useDispatch();
-    const categories = useSelector(state=> state=["smartPhone", "smartWatch","television"]);
+    const categories = useSelector(state=> state.productsReducer.categories);
     console.log(categories)
     const [category, setCategory] = useState({
         name: '',
     })
-    const [error, setError] = useState({
-        name: '',
-    })
-
+    
     useEffect(()=>{
-        dispatch(getCategories())
-        console.log(getCategories())
+        dispatch(getProductsCategories())
     },[dispatch])
 
     function handleSubmit(e){
-        dispatch(...categories,category)
+        dispatch(createCategory(category))
         setCategory({
             name: '',
         })
-        alert('Activity Created Succesfuly')
+        // alert('Category Created Succesfuly')
     }
     function handleChange(e){
-        let name= e.target.name;
-        let value= e.target.value;
-        if(value==""){
-            setError({...error,[name]:'No se admite campo vacio'})
-        }else if((name==="name" || name==="season")&& /\d/.test(value)){
-            setError({...error,[name]:'Solo se admiten letras'})
-        }else if(name!=="season" && name!=="name" && isNaN(value))
-            setError({...error,[name]:'Solo se admiten numeros'})
         setCategory({
-            ...category,
-            [e.target.name] : e.target.value,
+            ...categories,
+            name: e.target.value,
         })
     }
 
-    function handleSelect(e){
-        setCategory({
-            ...category,
-            name: e.target.value
-        })
-    }
-
-// function handleSelect(e){
-//     setCategory({
-//         ...category,
-
-//     })
-// }
 return(
 <>
 <div className={s.Background}>
     {/* <!-- AÑADIR COMPONENTE NAVBAR -->
     <!-- AÑADIR BOTON DESPLEGABLE PERFIL --> */}
-    <form className={s.Container}>
+    <form className={s.Container} >
         <div className={s.Title}> 
             <h2>Product Category Creation</h2>
         </div>
 
-    {/* <!-- CONECTAR CON ESTADOS DE CATEGORIAS --> */}
         <div className={s.InputSelect}>
             <div className={s.Input}>
                 <input 
-                    onChange={handleSelect}
+                    onChange={handleChange}
                     value={category.name}
                     name='name'
                     type="text"
