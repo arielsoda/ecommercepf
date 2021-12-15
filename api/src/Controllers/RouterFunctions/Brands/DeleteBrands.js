011496d095d490173f745ab5698e023dcde36d04
silcoro <aureliosilcoro@gmail.com>
2021-12-14 19:03:05 -0500

    deletebrands modified

--- api/src/Controllers/RouterFunctions/Brands/DeleteBrands.js
+++ api/src/Controllers/RouterFunctions/Brands/DeleteBrands.js
@@ -1,19 +1,39 @@
-const { Brand } = require('../../../db');
+const { Brand, Category, CategoryBrand,Product } = require('../../../db');
 
 const DeleteBrands = async (req, res, next) =>{
     try {
         const {id} = req.params;
-        console.log(`id`, id)
-        const bra = await Brand.findByPk(id);
+
+        //existe mi brands? x id
+        let bra = await Brand.findByPk(id);
+        
+        //si no existe devuelve un mensaje
         if (!bra) {
             res.status(404).json({
                 message:"no existe el Brands con ese id " + id
             })
-        };
-        await bra.destroy();
+        }else{//si existe
+            
+            //verifica que exista en la tabla CategoryBrand
+            let pruebabrand = await CategoryBrand.findOne({
+                where:{
+                    BrandId:bra.idBrand
+                }
+            })
 
+//si pruebabrand es null significa no encontro vinculo alguno
+            if (bra.idBrand && pruebabrand===null) {
+                // elimina el brands sin conexion
+                await bra.destroy();                
+                res.json(bra);
+            } else if(bra.idBrand === pruebabrand.BrandId){
+        // si existe y los 2 ID son iguales enviara un mensaje
+                res.json({
+                    message:'Brands No se Puede Eliminar porque esta vinculado con un Producto y una  Categoria'
+            })
+        }
+        }
         
-        res.json(bra);
     } catch (error) {
         next(error)
     }
