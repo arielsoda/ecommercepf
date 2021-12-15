de4d8f71ac941aafc3075830b83ca790e5eacad0
Ariel Sotelo <arielsoda14@gmail.com>
2021-12-15 17:03:26 -0300

    putCategory y deleteCategory

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -2,8 +2,12 @@ const {Router} = require('express');
 const router = Router();
 const {getCategories} = require('../Controllers/RouterFunctions/Categories/GetCategories');
 const {postCategory} = require('../Controllers/RouterFunctions/Categories/PostCategory');
+const {deleteCategory} = require('../Controllers/RouterFunctions/Categories/DeleteCategory');
+const { editCategory } = require('../Controllers/RouterFunctions/Categories/EditCategory');
 
 router.get('/', getCategories)
 router.post('/', postCategory)
+router.delete('/:id', deleteCategory)
+router.put('/:id', editCategory)
 
 module.exports= router;
