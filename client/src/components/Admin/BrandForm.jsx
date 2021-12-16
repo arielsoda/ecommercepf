435b95f091db1500d4f795c9d51bf33583684cad
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2021-12-16 13:36:46 -0600

    modificando gitignore

--- client/src/components/Admin/BrandForm.jsx
+++ client/src/components/Admin/BrandForm.jsx
@@ -1,4 +1,4 @@
-import { getBrands, createBrands, deleteBrand } from "../actions/index";
+import { getBrands, createBrands, deleteBrand } from "../../actions/index";
 import React,{useEffect, useState} from "react";
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
